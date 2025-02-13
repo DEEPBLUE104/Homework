7d067a464e79d78b073835480b494e7d97311e16
DEEPBLUE104 <dlara7173@gmail.com>
2025-02-13 17:44:13 +0900

    01_Java

--- 02_operater/src/edu/kh/op/practice/OperatorPractice.java
+++ 02_operater/src/edu/kh/op/practice/OperatorPractice.java
@@ -1,5 +1,6 @@
 package edu.kh.op.practice;
 
+// ctrl +shift + o : 자동 import 
 import java.util.Scanner;
 
 //기능 제공용 클래스
@@ -7,8 +8,8 @@ public class OperatorPractice {
 
 	public void practice1(){
 		
-		/*모든 사람이 사탕을 골고루 나눠가지려고 한다. 인원 수n와 사탕 개수m를 키보드로 입력 받고
-1인당 동일하게 나눠가진 사탕 개수a와 나눠주고 남은 사탕의 개수b를 출력하세요.
+		/*모든 사람이 사탕을 골고루 나눠가지려고 한다. 인원 수와 사탕 개수를 키보드로 입력 받고
+1인당 동일하게 나눠가진 사탕 개수와 나눠주고 남은 사탕의 개수를 출력하세요.
 
 [실행화면]
 인원 수 : 29
@@ -27,8 +28,10 @@ public class OperatorPractice {
 		
 		
 		System.out.println("1인당 사탕 개수: "+ (input2 / input1));
-
+	//	System.out.printf("1인당 사탕 개수: %d\n, input2 / input1);
+	
 		System.out.println("남는 사탕 개수: "+ (input2 - input1 * (input2 / input1)));
+	//	System.out.printf("남는 사탕 개수: %d\n, input2 % input1);
 		
 		
 	}
@@ -47,24 +50,40 @@ public class OperatorPractice {
 	System.out.print("이름: " );
 	String input1 = sc.nextLine();
 	
+	//String name = sc.next();
+	
 	System.out.print("학년: " );
 	int input2 = sc.nextInt();
 	
+	//int grade = sc.nextInt();
+	
 	System.out.print("반: " );
 	int input6 = sc.nextInt();
+
+	//int class = sc.nextInt();
 	
 	System.out.print("번호: " );
 	int input3 = sc.nextInt();
+
+    //	int number = sc.nextInt();
 	
-	sc.nextLine();
+	sc.nextLine(); // 입력버퍼 비우는 일 (개행문자 지우기)
 	
-	System.out.print("성별: " );
+	System.out.print("성별(남학생/여학생): " );
 	String input4 = sc.nextLine();
+
+	// String gender = sc.nextLine();
 	
 	System.out.print("성적: ");
 	double input5 = sc.nextDouble();
+
+	// double score = sc.nextDouble();
 	
 	System.out.println(input2 + "학년"+ input6 + "반"+ input3 +"번" + input1 + input4 + "의 성적은" + input5 + "이다.");
+    
+	//	System.out.printf(%d학년 %d반 %d번 %s %s의 성적은 %.2f이다.\n,
+	//					grade, classroom, number, name, gender, score);
+	
 	
 	}
 
@@ -81,16 +100,25 @@ public class OperatorPractice {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("국어: ");
-		int input1 = sc.nextInt();
+		int kor = sc.nextInt();
 		System.out.print("영어: ");
-		int input2 = sc.nextInt();
+		int eng = sc.nextInt();
 		System.out.print("수학: ");
-		int input3 = sc.nextInt();
+		int math = sc.nextInt();
 	
-		System.out.println("합계" + (input1 + input2 + input3));
-		System.out.println("평균" + (input1 + input2 + input3)/3);
 		
+		int sum = kor + eng + math; //합계
+		double avg = sum / 3.0; //평균
 		
+		System.out.println("합계 :" +sum );
+		System.out.printf("평균: %.1f\n", avg);
+	
+		//각 과목이 40점 이상, 평균 60점 이상이어야 합격
+		//그 외 불합격 출력
+		
+		boolean result = (kor >=40)&&(eng >= 40)&&(math>=40) && ( avg >=60);
+		
+		System.out.println( result ? "합격" : "불합격");
 	}
 
 }
---
+++ 03_control/src/edu/kh/control/practice/Run1.java
@@ -0,0 +1,14 @@
+package edu.kh.control.practice;
+
+public class Run1 {
+
+	public static void main(String[] args) {
+		
+		 ConditionPractice run= new ConditionPractice();
+		// run.practice1();
+		// run.practice2();
+		// run.practice3();
+		// run.practice4();
+		   run.practice5();
+	}
+}
